a403b33e2304b226d1bdb906de176a737b89d618
c-jaenicke <c-jaenicke@pm.me>
2022-02-15 15:32:16 +0100

    cleanup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from weakref import ref
 import requests
 import argparse
 
@@ -7,7 +8,7 @@ PAT_token = "TOKEN HERE"
 
 def make_request(endpoint):
     '''DONT USE
-    only here for future reference'''
+    only here for future reference and testing'''
     ApiUrlBase = "https://api.github.com/"
     ApiUrl = "{0}{1}".format(ApiUrlBase, endpoint)
 
@@ -30,7 +31,7 @@ def make_request(endpoint):
         return responseJson
 
 
-def request_repo_info(repo):
+def get_repo_info(repo):
     '''request info of a repo
 
     Args:
@@ -40,11 +41,9 @@ def request_repo_info(repo):
         responseJson: json containing data or the cause of error'''
     ApiUrlBase = "https://api.github.com/repos/"
     ApiUrl = "{0}{1}".format(ApiUrlBase, repo)
-
     headers = { "Content-Type": "application/json",
                 "Authorization": "token {0}".format(PAT_token),
     }
-
     response = requests.get(ApiUrl, headers=headers)
 
     if response.status_code == 200:
@@ -57,7 +56,7 @@ def request_repo_info(repo):
         return responseJson
 
 
-def request_repo_change(repo, changeFile):
+def patch_repo_change(repo, changeFile):
     '''request change of a repo
 
     Args:
@@ -68,14 +67,11 @@ def request_repo_change(repo, changeFile):
         responseJson: json containing data or the cause of error'''
     ApiUrlBase = "https://api.github.com/repos/"
     ApiUrl = "{0}{1}".format(ApiUrlBase, repo)
-
     headers = { "Content-Type": "application/json",
                 "Authorization": "token {0}".format(PAT_token)
     }
-
     # read changes to be made from json file
     changesJson = read_changes_from_file(changeFile)
-
     response = requests.patch(ApiUrl, data=changesJson, headers=headers, )
 
     if response.status_code == 200:
@@ -101,10 +97,8 @@ def post_create_repo(changeFile):
     headers = { "Content-Type": "application/json",
                 "Authorization": "token {0}".format(PAT_token)
     }
-
-    # read changes to be made from json file
+    # read new repo settings from file
     changesJson = read_changes_from_file(changeFile)
-
     response = requests.post(ApiUrlBase, data=changesJson, headers=headers, )
 
     if response.status_code == 200 or response.status_code == 201:
@@ -175,6 +169,7 @@ def write_info_to_file(repoName, responseJson):
 
 def read_changes_from_file(changeFile):
     '''reads file that contains the json formatted changes and returns them as a string
+
     Args:
         changeFile: name of the file that contains the values
         
@@ -182,23 +177,23 @@ def read_changes_from_file(changeFile):
         changesJson: string in json format'''
     file = open(changeFile,"r")
     changesJson = file.read()
-
     return changesJson
 
 
 def collect_info(repo):
+    '''connector for reading info to repos'''
     if "." in repo:
         file = open(repo)
         for line in file:
             line = line.strip("\n")
-            responseJson = request_repo_info(line)
+            responseJson = get_repo_info(line)
             if args.json:
                 write_info_to_file(line, responseJson)
             if args.view:
                 print_repo_info(line, responseJson)
         
     else: 
-        responseJson = request_repo_info(repo)
+        responseJson = get_repo_info(repo)
         if args.json:
                 write_info_to_file(repo, responseJson)
         if args.view:
@@ -206,54 +201,72 @@ def collect_info(repo):
 
 
 def make_changes(repo):
+    '''connector for making changes to repos'''
     if "." in repo:
         file = open(repo)
         for line in file:
             line = line.strip("\n")
-            responseJson = request_repo_change(line, args.changes)
+            responseJson = patch_repo_change(line, args.changes)
             if args.json:
                 write_info_to_file(line + "--changed", responseJson)
             if args.view:
                 print_repo_info(line, responseJson)
     
     else: 
-        responseJson = request_repo_change(repo, args.changes)
+        responseJson = patch_repo_change(repo, args.changes)
         if args.json:
                 write_info_to_file(repo + "--changed", responseJson)
         if args.view:
             print_repo_info(repo, responseJson)
 
-
-
-
-
-#def main():
-#   return
+# NOTE collect_info and make_changes can be put together in the future as parse_repo or something
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="GitHub Repo Normalizer" ,description="Normalize GitHub repo settings")
+    # build parser
+    parser = argparse.ArgumentParser(prog="GitHub Repo Normalizer" ,description="Normalize GitHub repo settings", formatter_class=argparse.RawTextHelpFormatter)
+
+    # string to be displayed for operation help
+    help_string = """Sets operation to be performed:
+    getinfo: returns info on the given repo, e.g. if its private and number of issues
+        Args: 
+            -repo <value> ; default = repos.txt ; takes a file to be iterated through or a single repo
+                repo needs to be formatted as "owner/repo_name", e.g. "c-jaenicke/repo_configurator"
+            -view ; default = false ; displays info about the repo in the terminal
+            -json ; default = false ; prints the info about the repo in a json file
+
+    changerepo: changes the settings of a repo
+        Args:
+            -repo <value> ; default = repos.txt ; takes a file to be iterated through or a single repo
+                repo needs to be formatted as "owner/repo_name", e.g. "c-jaenicke/repo_configurator"
+            -changes <value> ; default = changes.json ; specifies the file that will be read, which contains the changes to be made, see README.md for possible values
+
+    createrepo: creates a repo with the specified settings
+        Args:
+            -changes <value> ; default = changes.json ; specifies which settings the new repo will have, see README.md for possible values
+    """
+
     parser.add_argument("op",
                         choices=["getinfo", "changerepo", "createrepo"],
-                        help="""asdasdasdasd""")
+                        help=help_string)
 
     parser.add_argument("-repo", type=str,
                         default="repos.txt",
-                        help="provide the name of a file containing a list of repos, each in a new line")
+                        help="Default = repos.txt ; Reads a specified file for a list of repos, or takes a single repo, the names need to be formatted as \"owner/repo_name\" e.g. c-jaenicke/repo_configurator .")
 
     parser.add_argument("-json",
                         default=False,
                         action="store_true",
-                        help="When used the info about a repo will be saved in a .json")
+                        help="Default = False ;  When used the info about a repo will be stored in a .json file.")
 
     parser.add_argument("-view",
                         default=False,
                         action="store_true",
-                        help="When used the info about a repo will be printed into the console")
+                        help="Default = False ;  When used the info about a repo will be displayed in the terminal.")
 
     parser.add_argument("-changes", type=str,
                         default="changes.json",
-                        help="file containing the changes to be made to the repo in a JSON format. Default is changes.json. Please use https://jsonformatter.curiousconcept.com/ for testing your JSON")
+                        help="Default = changes.json ; Reads a specified file for changes to be made in a json format. Please use https://jsonformatter.curiousconcept.com/ for testing your JSON. Possible values are in the README.md.")
 
     args = parser.parse_args()
 
@@ -261,11 +274,17 @@ if __name__ == "__main__":
         collect_info(args.repo)
 
     elif args.op == "changerepo":
-        make_changes(args.repo)
+        # because changes can be disruptive, an extra step
+        confirm = input("--- CONFIRM THAT YOU WANT TO MAKE THESE CHANGES! y/n ---")
+        if confirm == "y" or confirm == "Y":
+            make_changes(args.repo)
+        else:
+            print("--- Stopping ... ---")
 
     elif args.op == "createrepo":
         post_create_repo(args.changes)
 
+    # triggers when no op is specified
     else:
         print("No valid operation entered, please use -h for help.")
 
