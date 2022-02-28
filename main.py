1c22a39c45aac03351dcf30419292753bcd8cd5a
c-jaenicke <c-jaenicke@pm.me>
2022-02-28 14:24:22 +0100

    Cleanup and added posting to gist

--- main.py
+++ main.py
@@ -1,113 +1,109 @@
 import json
-from weakref import ref
 import requests
 import argparse
+from datetime import date
+import sys
 
 # SET YOUR PERSONAL ACCESS TOKEN HERE
 PAT_token = "TOKEN HERE"
 
-def make_request(endpoint):
-    '''DONT USE
-    only here for future reference and testing'''
-    ApiUrlBase = "https://api.github.com/"
-    ApiUrl = "{0}{1}".format(ApiUrlBase, endpoint)
 
-    headers = { "Content-Type": "application/json",
-                "Authorization": "token {0}".format(PAT_token),
-    }
-
-    print(ApiUrl)
-    response = requests.get(ApiUrl, headers=headers)
-
-    if response.status_code == 200:
-        print("--- Successful")
-        responseJson = json.loads(response.content.decode("utf-8"))
-        #print(responseJson)
-        return responseJson
-    else:
-        print("--- Failed")
-        print(str(response.status_code))
-        responseJson = json.loads(response.content.decode("utf-8"))
-        return responseJson
-
-
-def get_repo_info(repo):
-    '''request info of a repo
-
-    Args:
-        repo: owner/name to a repo
-
-    Returns:
-        responseJson: json containing data or the cause of error'''
-    ApiUrlBase = "https://api.github.com/repos/"
-    ApiUrl = "{0}{1}".format(ApiUrlBase, repo)
-    headers = { "Content-Type": "application/json",
-                "Authorization": "token {0}".format(PAT_token),
-    }
-    response = requests.get(ApiUrl, headers=headers)
-
-    if response.status_code == 200:
-        print("--- Successful info request 200 for repo {0} ---".format(repo))
-        responseJson = json.loads(response.content.decode("utf-8"))
-        return responseJson
-    else:
-        print("--- Failed info request {0} for repo {1} ---".format(response.status_code, repo))
-        responseJson = json.loads(response.content.decode("utf-8"))
-        return responseJson
-
-
-def patch_repo_change(repo, changeFile):
-    '''request change of a repo
-
-    Args:
-        repo: owner/name to a repo
-        changeFile: file containing values to change in a json format
-
-    Returns:
-        responseJson: json containing data or the cause of error'''
-    ApiUrlBase = "https://api.github.com/repos/"
-    ApiUrl = "{0}{1}".format(ApiUrlBase, repo)
+def repo_requests(operation: str, repo: str, fileName=""):
+    ApiUrlBase = "https://api.github.com/repos"
+    ApiUrl = "{0}/{1}".format(ApiUrlBase, repo)
     headers = { "Content-Type": "application/json",
                 "Authorization": "token {0}".format(PAT_token)
     }
-    # read changes to be made from json file
-    changesJson = read_changes_from_file(changeFile)
-    response = requests.patch(ApiUrl, data=changesJson, headers=headers, )
 
-    if response.status_code == 200:
-        print("--- Successful repo change 200 for repo {0} ---".format(repo))
-        responseJson = json.loads(response.content.decode("utf-8"))
-        return responseJson
+    def get_info(url: str, headers: dict):
+        return requests.get(url, headers=headers)
+
+    def patch_change(url: str, headers: dict, fileName: str):
+        changesJson = read_file(fileName)
+        return requests.patch(url, data=changesJson, headers=headers)
+
+    def post_repo(headers: dict, fileName: str):
+        url = "https://api.github.com/user/repos"
+        repoSettings = str(read_file(fileName))
+        return requests.post(url, data=repoSettings, headers=headers)
+
+    if operation == "get":
+        response = get_info(ApiUrl, headers)
+
+    elif operation == "patch":
+        response = patch_change(ApiUrl, headers, fileName)
+
+    elif operation == "post":
+        response = post_repo(headers, fileName)
+
     else:
-        print("--- Failed repo change {0} for repo {1} ---".format(response.status_code, repo))
-        responseJson = json.loads(response.content.decode("utf-8"))
-        return responseJson
+        print("Invalid operation specified - Exit(0)")
+        sys.exit(1)
+
+    return process_response(response, repo)
 
 
-def post_create_repo(changeFile):
-    '''create a repo with defined values
 
-    Args:
-        changeFile: file containing values in json format
-
-    Returns:
-        responseJson: json containing data or the cause of error'''
-    ApiUrlBase = "https://api.github.com/user/repos"
-
-    headers = { "Content-Type": "application/json",
-                "Authorization": "token {0}".format(PAT_token)
+def gist_requests(operation: str, repo: str, fileName=""):
+    ApiUrlBase = "https://api.github.com/gists"
+    headers = { "Accept":"application/vnd.github.v3+json",
+                "Authorization":"token {0}".format(PAT_token)
     }
-    # read new repo settings from file
-    changesJson = read_changes_from_file(changeFile)
-    response = requests.post(ApiUrlBase, data=changesJson, headers=headers, )
 
-    if response.status_code == 200 or response.status_code == 201:
-        print("--- Successful created repo 200 ---")
+    def post_gist(url: str, headers: dict, fileName: str, repo: str):
+        content ={"description": "description",
+                "public": False,
+                "files": { 
+                    "RepoInfo": {
+                        "content": "fileContent"
+                    }
+                }
+        }
+        description = "Info for repo {0}".format(repo)
+        gistContent = read_file(fileName)
+        gistName = "RepoInfo--{0}--{1}".format(date.today().isoformat(), fileName)
+        content["description"] = description
+        content["files"] = {gistName: {"content": str(gistContent)}}
+        content = json.dumps(content,sort_keys=False, indent=4)
+        return requests.post(ApiUrlBase, data=content, headers=headers)
+
+    def get_gist():
+        # TODO
+        return
+
+    if operation == "post":
+        response = post_gist(ApiUrlBase, headers, fileName, repo)
+
+    elif operation == "post":
+        response = get_gist()
+        
+    else:
+        print("Invalid operation specified - Exit(0)")
+        sys.exit(1)
+
+    return process_response(response, repo)
+
+
+def process_response(response, repo: str):
+    if response.status_code == 200:
+        print("200 - Successful request for repo {0}".format(repo))
         responseJson = json.loads(response.content.decode("utf-8"))
         return responseJson
-    else:
-        print("--- Failed repo creation {0} ---".format(response.status_code))
+
+    elif response.status_code == 201:  
         responseJson = json.loads(response.content.decode("utf-8"))
+        print("201 - Successfully created - {0}".format(responseJson["html_url"]))
+        return responseJson
+
+    elif response.status_code == 422:  
+        responseJson = json.loads(response.content.decode("utf-8"))
+        print("422 - Unprocessable Entity for repo {0}\n\tError: {1}\n\tIt probably still worked, please check!".format(repo, responseJson))
+        return responseJson
+
+    else:
+        print("{0} - Failed request for repo {1}".format(response.status_code, repo))
+        responseJson = json.loads(response.content.decode("utf-8"))
+        print(responseJson)
         return responseJson
 
 
@@ -155,19 +151,21 @@ def print_repo_info(repo, data):
     ))
 
 
-def write_info_to_file(repoName, responseJson):
+def write_file(repoName, responseJson):
     '''write pretty json data to a file
 
     Args:
         repoName: name of the repo
         responseJson: json data returned by request_info/change_repo functions'''
     repoName = repoName.replace("/","-")
-    file = open(repoName + ".json", "w")
+    fileName = (repoName + ".json")
+    file = open(fileName, "w")
     file.write(json.dumps(responseJson, sort_keys=False, indent=4))
     file.close()
+    return fileName
 
 
-def read_changes_from_file(changeFile):
+def read_file(fileName):
     '''reads file that contains the json formatted changes and returns them as a string
 
     Args:
@@ -175,51 +173,53 @@ def read_changes_from_file(changeFile):
         
     Returns:
         changesJson: string in json format'''
-    file = open(changeFile,"r")
+    file = open(fileName,"r")
     changesJson = file.read()
     return changesJson
 
 
-def collect_info(repo):
-    '''connector for reading info to repos'''
+def get_repo_info(repo):
     if "." in repo:
         file = open(repo)
         for line in file:
             line = line.strip("\n")
-            responseJson = get_repo_info(line)
+            responseJson = repo_requests("get", line)
             if args.json:
-                write_info_to_file(line, responseJson)
+                write_file(line, responseJson)
             if args.view:
                 print_repo_info(line, responseJson)
+            if args.gist:
+                fileName = write_file(line, responseJson)
+                gist_requests("post", line, fileName)
         
     else: 
         responseJson = get_repo_info(repo)
         if args.json:
-                write_info_to_file(repo, responseJson)
+            write_file(repo, responseJson)
         if args.view:
             print_repo_info(repo, responseJson)
+        if args.gist:
+            fileName = write_file(repo, responseJson)
+            gist_requests("post", repo, fileName)
 
 
 def make_changes(repo):
-    '''connector for making changes to repos'''
     if "." in repo:
         file = open(repo)
         for line in file:
             line = line.strip("\n")
-            responseJson = patch_repo_change(line, args.changes)
+            responseJson = repo_requests("patch",line, args.changes)
             if args.json:
-                write_info_to_file(line + "--changed", responseJson)
+                write_file(line + "--changed", responseJson)
             if args.view:
                 print_repo_info(line, responseJson)
     
     else: 
-        responseJson = patch_repo_change(repo, args.changes)
+        responseJson = repo_requests("patch", repo, args.changes)
         if args.json:
-                write_info_to_file(repo + "--changed", responseJson)
+            write_file(repo + "--changed", responseJson)
         if args.view:
             print_repo_info(repo, responseJson)
-
-# NOTE collect_info and make_changes can be put together in the future as parse_repo or something
 
 
 if __name__ == "__main__":
@@ -234,6 +234,7 @@ if __name__ == "__main__":
                 repo needs to be formatted as "owner/repo_name", e.g. "c-jaenicke/repo_configurator"
             -view ; default = false ; displays info about the repo in the terminal
             -json ; default = false ; prints the info about the repo in a json file
+            -gist ; default = false ; when used the info about a repo will be uploaded in a private gist
 
     changerepo: changes the settings of a repo
         Args:
@@ -268,23 +269,27 @@ if __name__ == "__main__":
                         default="changes.json",
                         help="Default = changes.json ; Reads a specified file for changes to be made in a json format. Please use https://jsonformatter.curiousconcept.com/ for testing your JSON. Possible values are in the README.md.")
 
+    parser.add_argument("-gist",
+                        default=False,
+                        action="store_true",
+                        help="Default = False ;  When used the info about a repo will be uploaded in a private gist.")
+
     args = parser.parse_args()
 
     if args.op == "getinfo":
-        collect_info(args.repo)
+        get_repo_info(args.repo)
 
     elif args.op == "changerepo":
         # because changes can be disruptive, an extra step
-        confirm = input("--- CONFIRM THAT YOU WANT TO MAKE THESE CHANGES! y/n ---")
+        confirm = input("CONFIRM THAT YOU WANT TO MAKE THESE CHANGES! y/n ")
         if confirm == "y" or confirm == "Y":
             make_changes(args.repo)
         else:
-            print("--- Stopping ... ---")
+            print("Stopping ...")
 
     elif args.op == "createrepo":
-        post_create_repo(args.changes)
+        repo_requests("post","",args.changes)
 
     # triggers when no op is specified
     else:
         print("No valid operation entered, please use -h for help.")
-
